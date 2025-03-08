44f735bc5374d1e3630917c6653d34c3d3a9369d
Better-Great <abiegbeg@gmail.com>
2025-03-08 09:56:58 -0500

    updating edit-text branch

--- check-time.sh
+++ check-time.sh
@@ -8,3 +8,10 @@ current_time=$(date +"%H:%M:%S")
 
 # Display the current time
 echo "The current time in WAT is: $current_time"
+
+# Check if the current time is between 9:00 AM and 5:00 PM
+if [[ "$current_time" > "09:00:00" && "$current_time" < "17:00:00" ]]; then
+    echo "It is currently working hours."
+else
+    echo "It is currently outside of working hours."
+fi
